255fdd426a9aa3095c9812fb431d77cce477f9a3
zhiwen <zhiwen@smartx.com>
2019-11-27 19:03:47 +0800

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version='',
+    description='',
+    license='MIT Licence',
+    url='',
+    author='',
+    author_email='',
+    install_requires=['numpy>=0.17.0'],
+    packages=find_packages()
+)
